cf0ad25ce4fe7ca6af23f83aa6e756869414cc3a
ui-ux <ui-ux-developer@ukr.net>
2019-05-05 21:55:14 +0300

    base

---
+++ 00_base/00_base.js
@@ -0,0 +1,40 @@
+var a = 10;
+var b;
+var d;
+//String s = "hello";
+b = "Some string";
+
+e = 22;
+alert(e);
+
+var a = b;
+alert(a);
+
+var a = 10;
+var b = 5;
+var c = "test ";
+var d = "test2";
+var cd = c + d;
+var e = null;
+var r;
+var y = parseInt(y);
+var u = parseInt(u);
+var yu = y + u;
+var v = prompt("Enrer", "Enter");
+var conf = confirm("Ds edthtys ?");
+var i = 1;
+i +=3;
+document.write("<br/>i++ = " + i++);
+document.write("<br/>++i = " + ++i);
+document.write("<br/>i-- = " + i--);
+document.write("<br/>--i = " + --i);
+
+
+
+console.log("cd = " + cd);
+console.log("undefined = " + r );
+console.log(typeof(d));
+console.log(typeof(e));
+console.log("yu = " + yu);
+document.write("<br/>cd = " + cd);
+//document.write("<br/>"+ v);
