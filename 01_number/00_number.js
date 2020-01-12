b71f7d13f1044ed023df1d549325be38f0e9e1f2
ui-ux <ui-ux-developer@ukr.net>
2020-01-12 19:59:43 +0200

    base js update

---
+++ 01_number/00_number.js
@@ -0,0 +1,16 @@
+// In JavaScript, all numbers are stored in float64 format (8 bytes)
+/*
+NaN is not a number
+Number.MAX_VALUE - maximum allowed value in JavaScript +9007199254740992
+Number.MIN_VALUE - The minimum allowable value in JavaScript -9007199254740992
+Number.NaN - not a number
+Number.POSITIVE_INFINITY - + infinity 1.7976931348623157E + 10308
+Number.NEGATIVE_INFINITY - - infinity -1.7976931348623157E + 10308
+*/
+
+
+console.log("Number.NaN = " + Number.NaN);
+console.log("Number.MAX_VALUE = " + Number.MAX_VALUE);
+console.log("Number.MIN_VALUE = " + Number.MIN_VALUE);
+console.log("Number.POSITIVE_INFINITY = " + Number.POSITIVE_INFINITY);
+console.log("Number.NEGATIVE_INFINITY = " + Number.NEGATIVE_INFINITY);
